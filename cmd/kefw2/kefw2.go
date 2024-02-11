acc1d28f2c11d87f7928bf93b4adb0dc1a3bd7ec
Jens Hilligsøe <jens@hilli.dk>
2024-02-11 14:53:14 +0000

    chore: update year in license bits

--- cmd/kefw2/kefw2.go
+++ cmd/kefw2/kefw2.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 Jens Hilligsøe
+Copyright © 2023-2024 Jens Hilligsøe
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
